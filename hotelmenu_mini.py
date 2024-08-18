2fcb7263d57bd849d1fe8d66a4211bbd9d4db034
rampranav21 <110042708+rampranav21@users.noreply.github.com>
2024-08-18 22:25:54 +0530

    Updated hotelmenu_mini.py
    added loop statement

--- hotelmenu_mini.py
+++ hotelmenu_mini.py
@@ -1,34 +1,34 @@
-#Menu of the restaurant code
-hotelmenu ={
-    'Coffee':40,
-    'Pizza':60,
-    'Burger':65,
-    'Chilly paneer':70,
-    'Waffel':90,    
-    'Lassi':30,
+# Menu of the restaurant
+hotel_menu = {
+    'Coffee': 40,
+    'Pizza': 60,
+    'Burger': 65,
+    'Chilly Paneer': 70,
+    'Waffle': 90,    
+    'Lassi': 30,
 }
 
-print("Welcome to 5 star Restaurant")
-print("Coffee: Rs40\nPizza: Rs60\nBurger: Rs65\nChilly paneer: Rs70\nWaffel: Rs90\nLassi: Rs30,")
+print("Welcome to 5-Star Restaurant")
+print("Menu:")
+for item, price in hotel_menu.items():
+    print(f"{item}: Rs{price}")
 
-order_total=0
+order_total = 0
 
-item_1 = input("Enter the item you want to take")
-if item_1 in hotelmenu:
-    order_total += hotelmenu[item_1]
-    print(f"Your item{item_1} has been added to your order")
-
-else:
-    print(f"Ordered item {item_1} is not available yet")
-
-another_order = input("Do you want to add another item? (YES/NO)")
-if another_order == "YES":
-    item_2=input("Enter the name of second item")
-    if item_2 in hotelmenu:
-        order_total += hotelmenu[item_2]
-        print(f"Item{item_2} has been added to order")
+while True:
+    # Ask for an item to order
+    item = input("Enter the item you want to order: ")
+    if item in hotel_menu:
+        order_total += hotel_menu[item]
+        print(f"Your item '{item}' has been added to your order.")
     else:
-        print(f"Ordered item{item_2} is not available!")
+        print(f"Sorry, '{item}' is not available on the menu.")
     
-print(f"The total amount of your order{order_total}")
+    # Ask if they want to order another item
+    another_order = input("Do you want to add another item? (YES/NO): ").strip().upper()
+    if another_order != "YES":
+        break
+
+print(f"The total amount of your order is: Rs{order_total}")
+
 
